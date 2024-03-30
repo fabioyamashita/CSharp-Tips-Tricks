d62490cbb8ccc8b2a84d730adde63e1082ffde48
Fabio Yamashita <yamashitafabio@gmail.com>
2024-03-30 19:46:45 -0300

    feat: Comparison between string and StringBuilder

---
+++ string x StringBuilder/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace string_x_StringBuilder
+{
+    [MemoryDiagnoser(false)]
+    public class Methods
+    {
+        [Benchmark]
+        public string PrintString()
+        {
+            string str = "Hello";
+
+            for (int i = 0; i < 100; i++)
+            {
+                str += ", World!";
+            }
+
+            return str;
+        }
+
+        [Benchmark]
+        public StringBuilder PrintStringBuilder()
+        {
+            StringBuilder sb = new("Hello");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sb.Append(", World!");
+            }
+
+            return sb;
+        }
+    }
+}
+
+//| Method             |       Mean |     Error |    StdDev | Allocated |
+//| ------------------ | ---------: | --------: | --------: | --------: |
+//| PrintString        | 9,180.8 ns | 156.42 ns | 146.32 ns |  82.03 KB |
+//| PrintStringBuilder |   911.2 ns |  16.32 ns |  15.26 ns |   2.49 KB |
---
+++ string x StringBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using CommandLine;
+
+namespace string_x_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Methods = new Methods();
+            Console.WriteLine(Methods.PrintString());
+            Console.WriteLine(Methods.PrintStringBuilder());
+
+            var results = BenchmarkRunner.Run<Methods>();
+        }
+    }
+}
+
